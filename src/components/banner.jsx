210e34fafc7fe0c67c2a302c2a3666cd0e54470b
Paolo Landeros <landerospaolo@gmail.com>
2024-05-07 18:20:24 -0400

    banner

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,31 +1,17 @@
-import { useState, useEffect } from 'react'
 import video1 from '../videos/loop.mp4'
 import logo from '../imgs/affinitiLogo-removebg.png';
 import '../stylesheets/banner.css'
 
 function Banner() {
-    const [videoLoaded, setVideoLoaded] = useState(false);
-
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setVideoLoaded(true);
-        }, 2000);
-
-        return () => clearTimeout(timer);
-    })
-
     return (
         <div className="banner">
-            {videoLoaded ? (
-                <video className="video-background" autoPlay loop muted>
-                    <source src={video1} type="video/mp4" />
-                    Tu navegador no soporta video.
-                </video>
-            ) : null}
+            <video className="video-background" preload='auto' playsInline loop muted autoPlay>
+                <source src={video1} type="video/mp4" />
+                Tu navegador no soporta video.
+            </video>
             <div className='logo-container'>
                 <img className="logo" src={logo} alt="Affiniti" />
-                {/* <p className='text-center'>Brindamos soluciones para su empresa</p> */}
-                <p className='text-center'>Prueba!!!</p>
+                <p className='text-center'>Brindamos soluciones para su empresa</p>
             </div>
         </div>
     );
