12d242b6496dc610e881f173d7365db4e9f9b485
Paolo Landeros <landerospaolo@gmail.com>
2024-05-07 18:53:49 -0400

    prueba de gif en banner

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,14 +1,16 @@
 import video1 from '../videos/loop.mp4'
+import video2 from '../videos/eye-12452.gif'
 import logo from '../imgs/affinitiLogo-removebg.png';
 import '../stylesheets/banner.css'
 
 function Banner() {
     return (
         <div className="banner">
-            <video className="video-background" preload='auto' playsInline loop muted autoPlay>
-                <source src={video1} type="video/mp4" />
+            {/* <video className="video-background" preload='auto' playsInline loop muted autoPlay>
+                <source src={video2} type="video/mp4" />
                 Tu navegador no soporta video.
-            </video>
+            </video> */}
+            <img className="video-background "src={video2} alt="" />
             <div className='logo-container'>
                 <img className="logo" src={logo} alt="Affiniti" />
                 <p className='text-center'>Brindamos soluciones para su empresa</p>
