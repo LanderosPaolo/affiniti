481ab4d8c328a6160a7d521bd84ea3e77d41bd49
Paolo Landeros <landerospaolo@gmail.com>
2024-05-13 17:24:25 -0400

    estilo a seccion contacto

--- src/components/contacto.jsx
+++ src/components/contacto.jsx
@@ -1,27 +1,38 @@
 import '../stylesheets/contacto.css'
+import inicial from '../imgs/logoInicial-removebg-preview.png'
 
 function Contacto() {
     return (
-        <div className="container d-flex justify-content-around box">
-            <div>
-                <h5>Enviar mensaje</h5>
-                <h2 className="fw-bold">Contáctanos</h2>
-                <p className="fw-bold">Responderemos a la brevedad</p>
-            </div>
-            <div>
-                <div className="d-flex">
-                    <div>
-                        <i className="fa-regular fa-envelope mx-2"></i>
-                    </div>
-                    <p>contacto@affiniti.cl</p>
-                </div>
-                <div className="d-flex">
-                    <div>
-                        <i className="fa-solid fa-location-dot mx-2"></i>
-                    </div>
-                    <p>Guardia Vieja 202 Of 403 4P 403.<br></br>Providencia</p>
-                </div>
+        <div className='principal d-flex justify-content-evenly p-5'>
+            <div className='mensaje'>
                 <div>
+                    <p className='mensaje-contacto fw-bold'>Envíanos un mensaje</p>
+                    <h2 className='titulo-contacto fw-bold'>Ponte en contacto</h2>
+                    <p className='subtitulo-contacto fw-bold mb-5'>Te responderemos en breve.</p>
+                </div>
+                <div className='canales'>
+                    <div className='d-flex mb-4'>
+                        <div className='icono'>
+                            <i className="fa-regular fa-envelope"></i>
+                        </div>
+                        <p className='mx-2 mt-1'>contacto@affiniti.cl</p>
+                    </div>
+                    <div className='d-flex'>
+                        <div className='icono'>
+                            <i className="fa-solid fa-location-dot"></i>
+                        </div>
+                        <p className='mx-2'>Guardia Vieja 202 Of 403 4P 403.<br></br>Providencia</p>
+                    </div>
+                </div>
+            </div>
+            <div className='tarjeta-contacto'>
+                <div className='imgen-titulo text-center'>
+                    <div className='imagen bg-black'>
+                        <img src={inicial} alt="" />
+                    </div>
+                    <div className='titulo-tarjeta'>
+                        <h3><b>A</b>ffini<b>ti</b></h3>
+                    </div>
                 </div>
             </div>
         </div>
